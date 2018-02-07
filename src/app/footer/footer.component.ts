8bd513616310360da6ad30b0ca3beefc3fd7459c
Gerard <sylvaingerard4@hotmail.com>
2018-02-07 23:00:49 +0100

    add footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {NgModule, Component, OnInit } from '@angular/core';
+import { TemplateService } from '../template.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public templateService: TemplateService) { }
 
   ngOnInit() {
   }
-
+  public selectMenu(menu): void {
+    this.templateService.selectMenu(menu);
+  }
 }
